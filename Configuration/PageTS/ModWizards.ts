c08681f9b569dcd4ff9692f20c0904ed007a2aaa
Sebastian Fischer <typo3@evoweb.de>
2014-05-13 07:34:04 +0200

    Fix geocache persistence Modify templates Configure location cache to never expire Add location cache to clear cache all Add single location plugin
    Change-Id: I36259bb302691e0f3a74b43f46d4f692c69c8e21

--- Configuration/PageTS/ModWizards.ts
+++ Configuration/PageTS/ModWizards.ts
@@ -5,11 +5,22 @@ mod.wizards {
 				elements {
 					storefinder_map {
 						icon = ../typo3conf/ext/store_finder/Resources/Public/Icons/google-maps-icon.png
-						title = LLL:EXT:store_finder/Resources/Private/Language/locallang_be.xml:pi1_title
-						description = LLL:EXT:store_finder/Resources/Private/Language/locallang_be.xml:pi1_plus_wiz_description
+						title = LLL:EXT:store_finder/Resources/Private/Language/locallang_be.xml:pi1_title_map
+						description = LLL:EXT:store_finder/Resources/Private/Language/locallang_be.xml:pi1_description_map
 						tt_content_defValues {
 							CType = list
 							list_type = storefinder_map
+						}
+					}
+				}
+				elements {
+					storefinder_show {
+						icon = ../typo3conf/ext/store_finder/Resources/Public/Icons/google-maps-icon.png
+						title = LLL:EXT:store_finder/Resources/Private/Language/locallang_be.xml:pi1_title_show
+						description = LLL:EXT:store_finder/Resources/Private/Language/locallang_be.xml:pi1_description_show
+						tt_content_defValues {
+							CType = list
+							list_type = storefinder_show
 						}
 					}
 				}
