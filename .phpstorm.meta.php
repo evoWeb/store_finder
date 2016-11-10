5d853532f2c76ed6901dfb8a55335768e9eba607
Sebastian Fischer <typo3@evoweb.de>
2016-11-10 09:15:23 +0100

    Fix phpstorm classname usage for phplint

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,15 +1,18 @@
 <?php
 
-namespace PHPSTORM_META {
-	/** @noinspection PhpUnusedLocalVariableInspection */
-	/** @noinspection PhpIllegalArrayKeyTypeInspection */
-	/** @noinspection PhpDynamicAsStaticMethodCallInspection */
-	$STATIC_METHOD_TYPES = [
-		\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('') => [
-			'Evoweb\\StoreFinder\\Service\\GeocodeService' instanceof \Evoweb\StoreFinder\Service\GeocodeService,
-		],
-		\TYPO3\CMS\Extbase\Object\ObjectManager::get('') => [
-			'Evoweb\\StoreFinder\\Service\\GeocodeService' instanceof \Evoweb\StoreFinder\Service\GeocodeService,
-		],
-	];
+$serviceClassname = 'Evoweb\\StoreFinder\\Service\\GeocodeService';
+
+namespace PHPSTORM_META
+{
+    /** @noinspection PhpUnusedLocalVariableInspection */
+    /** @noinspection PhpIllegalArrayKeyTypeInspection */
+    /** @noinspection PhpDynamicAsStaticMethodCallInspection */
+    $STATIC_METHOD_TYPES = [
+        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('') => [
+            $serviceClassname instanceof \Evoweb\StoreFinder\Service\GeocodeService,
+        ],
+        \TYPO3\CMS\Extbase\Object\ObjectManager::get('') => [
+            $serviceClassname instanceof \Evoweb\StoreFinder\Service\GeocodeService,
+        ],
+    ];
 }
