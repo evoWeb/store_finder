c58ac131b835d0e5415e9bb1e60117fe94de39a3
Sebastian Fischer <typo3@evoweb.de>
2018-06-13 11:01:09 +0200

    Mitigate collition with ext:news
    Ext:news uses a slightly different field definition. To prevent
    database compare loops this fix modifies the definition to
    match Ext:news.

--- ext_tables.sql
+++ ext_tables.sql
@@ -127,5 +127,5 @@ CREATE TABLE tx_storefinder_location_location_mm (
 #
 CREATE TABLE sys_category (
 	children int(11) unsigned DEFAULT '0' NOT NULL,
-	import_id int(11) DEFAULT '0' NOT NULL
+	import_id varchar(100) DEFAULT '' NOT NULL
 );
