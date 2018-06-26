da32733d72a95905b9b42cd07f9884dba054d91d
Sebastian Fischer <typo3@evoweb.de>
2018-06-26 07:29:45 +0200

    Increase compat

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['store_finder'] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.2.99',
+            'typo3' => '8.7.0-9.3.99',
             'static_info_tables' => '6.5.1-',
         ],
         'conflicts' => [],
