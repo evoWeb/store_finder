e42fe5008321bddcb08361dd758cd2261519e500
Sebastian Fischer <typo3@evoweb.de>
2018-11-01 20:24:41 +0100

    Improve local tests

--- .Build/test.sh
+++ .Build/test.sh
@@ -1,12 +1,14 @@
+
 #!/usr/bin/env bash
+
+export PACKAGE="evoWeb/store_finder";
+export T3EXTENSION="store_finder";
 
 runFunctionalTests () {
     PHP=${1};
-    PACKAGE=${2};
-    T3EXTENSION=${3};
-    TYPO3_VERSION=${4};
-    TESTING_FRAMEWORK=${5};
-    DB_DRIVER=${6};
+    TYPO3_VERSION=${2};
+    TESTING_FRAMEWORK=${3};
+    DB_DRIVER=${4};
     COMPOSER="/usr/local/bin/composer";
 
     rm -rf ./*
@@ -29,7 +31,7 @@ runFunctionalTests () {
     echo "Running php lint";
     errors=$(find . -name \*.php ! -path "./.Build/*" -exec ${PHP} -d display_errors=stderr -l {} 2>&1 >/dev/null \;) && echo "$errors" && test -z "$errors"
 
-    echo "Running xmllint (Xliff)";
+    echo "Running xmllint (Xliff) (Remember to install libxml2-utils)";
     find Resources/Private/Language/ -name '*.xlf' -type f | xargs xmllint --noout --schema Tests/Fixtures/xliff-core-1.2-strict.xsd
 
     echo "Running functional tests"; \
@@ -44,7 +46,14 @@ runFunctionalTests () {
 
     rm composer.lock
     rm -rf .Build/Web/
+    rm -rf .Build/bin/
     rm -rf var/
+
+    cd ../../
 }
 
-runFunctionalTests "/usr/bin/php7.2" "evoWeb/store_finder" "store_finder" "^9.5.0" "~4.10.0" "pdo_sqlite";
+runFunctionalTests "/usr/bin/php7.0" "^8.7.0" "~1.3.0" "mysqli";
+runFunctionalTests "/usr/bin/php7.1" "^8.7.0" "~1.3.0" "mysqli";
+runFunctionalTests "/usr/bin/php7.2" "^8.7.0" "~1.3.0" "mysqli";
+runFunctionalTests "/usr/bin/php7.2" "^9.5.0" "~4.10.0" "pdo_sqlite";
+runFunctionalTests "/usr/bin/php7.2" "dev-master as 10.0.0" "~4.10.0" "pdo_sqlite";
