606c7bec7f0e73889f9da4b07cf55b5244d44fe5
Sebastian Fischer <typo3@evoweb.de>
2018-11-13 20:12:02 +0100

    Refactor StoreFinderMap to be constructor and class methods

--- Resources/Public/JavaScript/map-osm.js
+++ Resources/Public/JavaScript/map-osm.js
@@ -1,252 +1,247 @@
 /* global window, define, jQuery */
-(function(factory) {
-	'function' === typeof define && define.amd ? define('map', ['jquery', 'window'], factory) : factory(jQuery, window)
-})(function($, root) {
-	var StoreFinderMap = {};
+(function (factory) {
+  'function' === typeof define && define.amd ? define('map', ['jquery', 'window'], factory) : factory(jQuery, window)
+})(function ($, root) {
+  var L; function StoreFinderMap(mapConfiguration, locations) {
+    this.map = null;
+    this.mapConfiguration = mapConfiguration || {
+      active: false,
+      afterSearch: 0,
+      apiConsoleKey: '',
+      apiUrl: '',
+      apiV3Layers: '',
+      kmlUrl: '',
+      language: 'en',
+      allowSensors: false,
+      renderSingleViewCallback: null,
+      handleCloseButtonCallback: null
+    };
+    this.locations = locations || [];
+    this.infoWindow = null;
+    this.infoWindowTemplate = null;
 
-	var L,
-		map,
-		mapConfiguration = root.mapConfiguration || {
-			active: false,
-			apiV3Layers: '',
-			language: 'en',
-			allowSensors: false,
-			renderSingleViewCallback: null,
-			handleCloseButtonCallback: null
-		},
-		locations = root.locations || [],
-		infoWindow = null,
-		infoWindowTemplate;
+    this.loadScript();
+  }
 
-	/**
-	 * Initialize information layer on map
-	 *
-	 * @return void
-	 */
-	StoreFinderMap.initializeLayer = function() {
-		'use strict';
-		if (mapConfiguration.apiV3Layers.indexOf('traffic') > -1) {
-			var trafficLayer = new google.maps.TrafficLayer();
-			trafficLayer.setMap(map);
-		}
+  /**
+   * Initialize information layer on map
+   */
+  StoreFinderMap.initializeLayer = function () {
+    'use strict';
+    if (this.mapConfiguration.apiV3Layers.indexOf('traffic') > -1) {
+      var trafficLayer = new google.maps.TrafficLayer();
+      trafficLayer.setMap(this.map);
+    }
 
-		if (mapConfiguration.apiV3Layers.indexOf('bicycling') > -1) {
-			var bicyclingLayer = new google.maps.BicyclingLayer();
-			bicyclingLayer.setMap(map);
-		}
+    if (this.mapConfiguration.apiV3Layers.indexOf('bicycling') > -1) {
+      var bicyclingLayer = new google.maps.BicyclingLayer();
+      bicyclingLayer.setMap(this.map);
+    }
 
-		if (mapConfiguration.apiV3Layers.indexOf('panoramio') > -1) {
-			var panoramioLayer = new google.maps.panoramio.PanoramioLayer();
-			panoramioLayer.setMap(map);
-		}
+    if (this.mapConfiguration.apiV3Layers.indexOf('panoramio') > -1) {
+      var panoramioLayer = new google.maps.panoramio.PanoramioLayer();
+      panoramioLayer.setMap(this.map);
+    }
 
-		if (mapConfiguration.apiV3Layers.indexOf('weather') > -1) {
-			var weatherLayer = new google.maps.weather.WeatherLayer({
-				temperatureUnits: google.maps.weather.TemperatureUnit.DEGREE
-			});
-			weatherLayer.setMap(map);
-		}
+    if (this.mapConfiguration.apiV3Layers.indexOf('weather') > -1) {
+      var weatherLayer = new google.maps.weather.WeatherLayer({
+        temperatureUnits: google.maps.weather.TemperatureUnit.DEGREE
+      });
+      weatherLayer.setMap(this.map);
+    }
 
-		if (mapConfiguration.apiV3Layers.indexOf('kml') > -1) {
-			var kmlLayer = new google.maps.KmlLayer(mapConfiguration.kmlUrl);
-			kmlLayer.setMap(map);
-		}
-	};
+    if (this.mapConfiguration.apiV3Layers.indexOf('kml') > -1) {
+      var kmlLayer = new google.maps.KmlLayer(this.mapConfiguration.kmlUrl);
+      kmlLayer.setMap(this.map);
+    }
+  };
 
-	/**
-	 * Initialize info window template
-	 */
-	StoreFinderMap.initializeTemplates = function() {
-		'use strict';
+  /**
+   * Initialize info window template
+   */
+  StoreFinderMap.prototype.initializeTemplates = function () {
+    'use strict';
 
-		var source = $('#templateInfoWindow').html();
-		infoWindowTemplate = Hogan.compile(source);
+    var self = this;
+    self.infoWindowTemplate = Hogan.compile($('#templateInfoWindow').html());
 
-		$(document).on('click', '.tx-storefinder .infoWindow .close', function() {
-			var $closeButton = $(this);
+    $(document).on('click', '.tx-storefinder .infoWindow .close', function () {
+      var $closeButton = $(this);
 
-			if (typeof mapConfiguration.insertSingleViewInto !== 'undefined') {
-				alert('Using configuration.insertSingleViewInto is deprecated please use configuration.handleCloseButtonCallback instead');
-			} else if (typeof configuration.renderSingleViewCallback === 'function') {
-				configuration.handleCloseButtonCallback($closeButton);
-			} else {
-				infoWindow.close();
-			}
-		});
-	};
+      if (typeof self.mapConfiguration.renderSingleViewCallback === 'function') {
+        self.mapConfiguration.handleCloseButtonCallback($closeButton);
+      } else {
+        self.infoWindow.close();
+      }
+    });
+  };
 
-	/**
-	 * Initialize instance of map infoWindow
-	 */
-	StoreFinderMap.initializeInfoWindow = function() {
-		'use strict';
+  /**
+   * Initialize instance of map infoWindow
+   */
+  StoreFinderMap.prototype.initializeInfoWindow = function () {
+    'use strict';
 
-		infoWindow = L.popup();
-	};
+    this.infoWindow = L.popup();
+  };
 
-	/**
-	 * Close previously open info window, renders new content and opens the window
-	 */
-	StoreFinderMap.showInformation = function() {
-		'use strict';
+  /**
+   * Close previously open info window, renders new content and opens the window
+   *
+   * @param {object} marker
+   */
+  StoreFinderMap.prototype.showInformation = function (marker) {
+    'use strict';
 
-		var marker = this,
-			location = marker.sfLocation;
+    var location = marker.sfLocation;
 
-		if (typeof mapConfiguration.insertSingleViewInto !== 'undefined') {
-			alert('Using configuration.insertSingleViewInto is deprecated please use configuration.renderSingleViewCallback instead');
-		} else if (typeof mapConfiguration.renderSingleViewCallback === 'function') {
-			mapConfiguration.renderSingleViewCallback(location, infoWindowTemplate);
-		} else {
-			if (infoWindow.isOpen()) {
-				infoWindow.closePopup();
-			}
+    if (typeof this.mapConfiguration.renderSingleViewCallback === 'function') {
+      this.mapConfiguration.renderSingleViewCallback(location, this.infoWindowTemplate);
+    } else {
+      if (this.infoWindow.isOpen()) {
+        this.infoWindow.closePopup();
+      }
 
-			infoWindow = marker.getPopup()
-				.setLatLng(L.latLng(location.lat, location.lng))
-				.setContent(infoWindowTemplate.render(location.information))
-				.openOn(map);
-		}
-	};
+      this.infoWindow = marker.getPopup()
+        .setContent(this.infoWindowTemplate.render(location.information))
+        .setLatLng(L.latLng(location.lat, location.lng))
+        .openOn(this.map);
+    }
+  };
 
-	/**
-	 * Trigger click event on marker on click in result list
-	 *
-	 * @param {Integer} index
-	 */
-	StoreFinderMap.openInfoWindow = function(index) {
-		'use strict';
+  /**
+   * Trigger click event on marker on click in result list
+   *
+   * @param {number} index
+   */
+  StoreFinderMap.prototype.openInfoWindow = function (index) {
+    'use strict';
 
-		locations[index].marker.click();
-	};
+    this.locations[index].marker.click();
+  };
 
-	/**
-	 * Initialize location marker on map
-	 */
-	StoreFinderMap.initializeLocation = function() {
-		'use strict';
+  /**
+   * Initialize location marker on map
+   */
+  StoreFinderMap.prototype.initializeLocation = function () {
+    'use strict';
 
-		var index, location;
-		if (locations.length) {
-			for (index = 0; index < locations.length; index++) {
-				location = locations[index];
-				location['information']['index'] = index;
+    var self = this;
+    self.locations.map(function (location, index) {
+      location['information']['index'] = index;
 
-				var marker = L.marker([location.lat, location.lng], {
-					title: location.name
-				}).bindPopup('').addTo(map);
+      var marker = L.marker([location.lat, location.lng], {
+        title: location.name
+      }).bindPopup('').addTo(self.map);
 
-				marker.sfLocation = location;
+      marker.sfLocation = location;
 
-				marker.on('click', StoreFinderMap.showInformation);
+      marker.on('click', function () { self.showInformation(this); });
 
-				// attach marker to location to be able to close it later
-				location.marker = marker;
-			}
-		}
-	};
+      // attach marker to location to be able to close it later
+      location.marker = marker;
+    });
+  };
 
-	/**
-	 * Initialize map
-	 */
-	StoreFinderMap.initializeMap = function() {
-		'use strict';
+  /**
+   * Initialize map
+   */
+  StoreFinderMap.prototype.initializeMap = function () {
+    'use strict';
+    var self = this;
+    L = root.L;
+    self.map = L.map('tx_storefinder_map');
 
-		L = root.L;
-		map = L.map('tx_storefinder_map');
+    if (typeof mapConfiguration.center !== 'undefined') {
+      map.setView(
+        [self.mapConfiguration.center.lat, self.mapConfiguration.center.lng],
+        parseInt(self.mapConfiguration.zoom, 10)
+      );
+    } else {
+      map.setView([0, 0], 13);
+    }
 
-		if (typeof mapConfiguration.center !== 'undefined') {
-			map.setView(
-				[mapConfiguration.center.lat, mapConfiguration.center.lng],
-				parseInt(mapConfiguration.zoom, 10)
-			);
-		} else {
-			map.setView([0, 0], 13);
-		}
+    // more providers can be found here http://leaflet-extras.github.io/leaflet-providers/preview/
+    L.tileLayer(
+      'https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    ).addTo(self.map);
+  };
 
-		// more providers can be found here http://leaflet-extras.github.io/leaflet-providers/preview/
-		L.tileLayer(
-			'https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
-				maxZoom: 20,
-				attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-			}
-		).addTo(map);
-	};
+  /**
+   * Initialize list click events
+   */
+  StoreFinderMap.prototype.initializeListEvents = function () {
+    'use strict';
+    var self = this;
+    $(document).on('click', '.tx-storefinder .resultList > li', function () {
+      self.openInfoWindow($(this).data('index'));
+    });
+  };
 
-	/**
-	 * Initialize list click events
-	 */
-	StoreFinderMap.initializeListEvents = function() {
-		'use strict';
+  /**
+   * Initialize map
+   */
+  StoreFinderMap.prototype.postLoadScript = function () {
+    'use strict';
+    this.initializeMap();
+    this.initializeLayer();
+    this.initializeLocation();
+    this.initializeInfoWindow();
+    this.initializeTemplates();
+    this.initializeListEvents();
+  };
 
-		$(document).on('click', '.tx-storefinder .resultList > li', function() {
-			StoreFinderMap.openInfoWindow($(this).data('index'));
-		});
-	};
+  /**
+   * Load open street map leaflet script
+   */
+  StoreFinderMap.prototype.loadScript = function () {
+    'use strict';
 
-	/**
-	 * Initialize map
-	 */
-	StoreFinderMap.postLoadScript = function() {
-		'use strict';
+    var self = this;
+    var $css = $.Deferred(),
+      $cssFile = $('<link/>', {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        integrity: 'sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==',
+        crossorigin: ''
+      }).appendTo('head');
+    $css.resolve($cssFile);
 
-		StoreFinderMap.initializeMap();
-		//StoreFinderMap.initializeLayer();
-		StoreFinderMap.initializeLocation();
-		StoreFinderMap.initializeInfoWindow();
-		StoreFinderMap.initializeTemplates();
-		StoreFinderMap.initializeListEvents();
-	};
+    var $js = $.Deferred(),
+      $jsFile = $('<script/>', {
+        src: 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+        integrity: 'sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==',
+        crossorigin: ''
+      }).appendTo('head');
+    $js.resolve($jsFile);
 
-	/**
-	 * Load open street map leaflet script
-	 */
-	StoreFinderMap.loadScript = function() {
-		'use strict';
+    $.when(
+      $css.promise(),
+      $js.promise()
+    ).done(function () {
+      var interval = setInterval(function () {
+        if (typeof root.L !== 'undefined') {
+          root.clearInterval(interval);
+          self.postLoadScript();
+        }
+      }, 10);
+    }).fail(function () {
+        console.log('Failed loading osm resources.');
+    });
+  };
 
-		var $css = $.Deferred();
-		var $cssFile = $('<link/>', {
-			rel: 'stylesheet',
-			type: 'text/css',
-			href: 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-			integrity: 'sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==',
-			crossorigin: ''
-		}).appendTo('head');
-		$css.resolve($cssFile);
+  $(document).ready(function () {
+    'use strict';
 
-		var $js = $.Deferred();
-		var $jsFile = $('<script/>', {
-			src: 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-			integrity: 'sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==',
-			crossorigin: ''
-		}).appendTo('head');
-		$js.resolve($jsFile);
+    if (root.mapConfiguration.active) {
+      // make module public to be available for callback after load
+      root.StoreFinder = new StoreFinderMap(root.mapConfiguration, root.locations);
+    }
+  });
 
-		$.when($css.promise(), $js.promise()).then(
-			function() {
-				var interval = setInterval(function() {
-					if (typeof root.L !== 'undefined') {
-						root.clearInterval(interval);
-						StoreFinderMap.postLoadScript();
-					}
-				}, 10);
-			},
-			function () {
-				console.log('Failed loading osm resources.');
-			}
-		);
-	};
-
-	$(document).ready(function() {
-		'use strict';
-
-		// make StoreFinderMap public to be available for callback after load
-		root.StoreFinder = StoreFinderMap;
-
-		if (mapConfiguration.active) {
-			StoreFinderMap.loadScript();
-		}
-	});
-
-	return StoreFinderMap;
+  return StoreFinderMap;
 });
