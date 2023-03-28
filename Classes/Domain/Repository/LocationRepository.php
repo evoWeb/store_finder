79a5bdbe14dfc8aa78904ab65c77b8c99dffce78
Sebastian Fischer <typo3@evoweb.de>
2023-03-28 15:51:16 +0200

    Add requirement

--- Classes/Domain/Repository/LocationRepository.php
+++ Classes/Domain/Repository/LocationRepository.php
@@ -24,7 +24,6 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Generic\Query;
-use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
